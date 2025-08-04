7e58605f565ae9658a308ad9c6494bb0f0bcb535
YuriyM <Mikchailov1988@mail.ru>
2025-08-04 16:46:59 +0300

    attempt 1

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,60 @@ public class Main {
 
         System.out.println("Задача 1");
 
+        for (int i = 1; i < 11; i++ ) {
+            System.out.println("Целые числа " + i);
+        }
+
+        System.out.println("Задача 2");
+
+        for (int i = 10; i > 0; i--) {
+            System.out.println("Целые числа наоборот " + i);
+        }
+
+        System.out.println("Задача 3");
+
+        for (int i = 0; i < 17; i = i + 2) {
+            System.out.println("Четные числа " + i);
+        }
+
+        System.out.println("Задача 4");
+
+        for (int i = 10; i > -11; i--) {
+            System.out.println("От большего числа к меньшему " + i);
+        }
+
+        System.out.println("Задача 5");
+
+        for (int i = 1904; i < 2100; i = i + 4) {
+            System.out.println( i + " год является високосным");
+        }
+
+        System.out.println("Задача 6");
+
+        for (int i = 7; i < 105; i = i + 7) {
+            System.out.println("Последовательность чисел " + i);
+        }
+
+        System.out.println("Задача 7");
+
+        int number = 1;
+        for (int i = 0; i < 10; i++ ) {
+            System.out.println("Последовательность чисел " + number);
+            number = number * 2;
+        }
+
+        System.out.println("Задача 8");
+
+        int save = 29000;
+        int total = 0;
+        for (int i = 1; i <= 12; i++) {
+            total = total + save;
+            System.out.println( "Месяц " + i + ", сумма накоплений равна " + total + " рублей" );
+        }
+        System.out.println("Сумма всего накопленных за 12 месяцев " + total + " рублей");
+
+        System.out.println("Задача 9");
+
 
     }
 }
